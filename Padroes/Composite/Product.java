6aa4a307777e4e9d1e0e78541706b773b6dabf4a
José Henrique Cz. dos Santos <49040824+josehcz@users.noreply.github.com>
2022-06-01 21:56:33 -0300

    Update Product.java

--- Padroes/Composite/Product.java
+++ Padroes/Composite/Product.java
@@ -1,9 +1,5 @@
 package Composite;
 
-// É nossa estrutura de concreto.
-// Corresponde à parte Leaf no diagrama UML.
-// Representa a camada mais baixa da hierarquia.
-
 class Product implements ICatalogComponent {
     private String _name;
 
